e97734229dbd4b334537124558bb14555e020783
jhosepr <rpj1010465@gmail.com>
2026-09-11 22:03:23 -0400

    docs: agregar comentarios sobre la conexion a base de datos

--- login.py
+++ login.py
@@ -3,3 +3,4 @@ def validar_usuario(user, pass): return True
 # TODO: Conectar con base de datos de pacientes del hospital
 # Seguridad: Encriptar contraseñas antes de enviar
 def validar_usuario(user, pass): return True
+# TODO: Conectar con base de datos de pacientes del hospital
